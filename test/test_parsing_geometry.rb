e75bb5e910b3ef2f3feb82470ad1c6d0bfc1db33
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-02 23:53:04 +1300

    Extensive geometry loading test.

--- test/test_parsing_geometry.rb
+++ test/test_parsing_geometry.rb
@@ -165,12 +165,39 @@ class TestParsingGeometry < Test::Unit::TestCase
 		
 		expected = [
 			[
-				Attribute.position(:X=>1.0, :Y=>1.0, :Z=>-1.0),
-				Attribute.normal(:X=>0.0, :Y=>0.0, :Z=>1.0)
-			]
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>-1.0})],
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>-1.0})],
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>-1.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>-1.0})]],
+			[
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>1.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>1.0})],
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>1.0})],
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>0.0, :Z=>1.0})]],
+			[
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>1.0, :Y=>-0.0, :Z=>0.0})],
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>1.0, :Y=>-0.0, :Z=>0.0})],
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>1.0, :Y=>-0.0, :Z=>0.0})],
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>1.0, :Y=>-0.0, :Z=>0.0})]],
+			[
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>-0.0, :Y=>-1.0, :Z=>0.0})],
+				[Attribute.position({:X=>1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>-0.0, :Y=>-1.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>-0.0, :Y=>-1.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>-0.0, :Y=>-1.0, :Z=>0.0})]],
+			[
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>-1.0}), Attribute.normal({:X=>-1.0, :Y=>0.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>-1.0, :Z=>1.0}), Attribute.normal({:X=>-1.0, :Y=>0.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>-1.0, :Y=>0.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>-1.0, :Y=>0.0, :Z=>0.0})]],
+			[
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>1.0, :Z=>0.0})],
+				[Attribute.position({:X=>1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>1.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>-1.0}), Attribute.normal({:X=>0.0, :Y=>1.0, :Z=>0.0})],
+				[Attribute.position({:X=>-1.0, :Y=>1.0, :Z=>1.0}), Attribute.normal({:X=>0.0, :Y=>1.0, :Z=>0.0})]]
 		]
 		
-		assert_equal :vertex, mesh.polygons.inputs.ordered[0].semantic
-		assert_equal expected[0], mesh.polygons.vertex(0)
+		mesh.polygons.each_with_index do |polygon, index|
+			assert_equal expected[index], polygon
+		end
 	end
 end
